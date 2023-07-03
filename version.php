b9283d30fbfde064852c1cccec172150159f06b8
Alex Walker <a.walker43@rgu.ac.uk>
2023-07-03 18:24:07 +0100

    VLE-6: bumped version.php files for release

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022090904;
+$plugin->version   = 2023070300;
 $plugin->requires  = 2022041200;
 $plugin->component = 'theme_garthdee';
